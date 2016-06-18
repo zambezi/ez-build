0581d9e3103ab4bbbfb7415e4e22d7d0f0adc8db
Marcus Stade <marcus@stade.se>
2016-06-19 00:33:28 +0200

    Simplify performance timer

--- src/util/performance.js
+++ src/util/performance.js
@@ -1,16 +1,13 @@
-import { is } from 'funkis'
+import { val } from 'funkis'
 
-export async function timed(fn) {
-  if (!is(Function, fn)) {
-    fn = Promise.resolve.bind(Promise, fn)
-  }
-
+export async function timed(proc) {
   const start = process.hrtime()
-  const result = await(fn())
+  const result = await Promise.resolve(val(proc))
   const duration = process.hrtime(start)
-  const ms = (duration[0] * 1e9 + duration[1]) / 1e6
 
-  return { result, duration:
-    { ms }
-  }
+  duration.ns = duration[0] * 1e9 + duration[1]
+  duration.ms = duration.ns / 1e6
+  duration.s  = duration.ns / 1e9
+
+  return { result, duration }
 }
