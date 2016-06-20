696d0c131adb5228ff0f15853eb53dc0f03c1319
Marcus Stade <marcus@stade.se>
2016-06-20 18:40:04 +0200

    Use `slurp` in copy-files and remove `mz` dependency

--- src/builder/copy-files.js
+++ src/builder/copy-files.js
@@ -1,9 +1,9 @@
-import { readFile } from 'mz/fs'
+import { slurp } from '../util/file'
 import { extname } from 'path'
 
 export default function configure(pkg, opts) {
   return async function process(name, input) {
-    let data = await readFile(input)
+    let data = await slurp(input)
     return { files: { [`${name}${extname(input)}`]: data } }
   }
 }
