39e8b501cc0d7bb228fa731f28412f1b8413c1ed
Marcus Stade <marcus@stade.se>
2016-08-15 20:45:20 +0200

    Use bats for testing, and add some common scaffolding
    The setup.bash file is intended to be loaded from test suites
    using the bats `load` feature.

---
+++ test/setup.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+project_dirname=$(readlink -m ./)
+bin="${project_dirname}/bin/ez-build.js"
+
+ez-build() {
+  echo "argc: ${bin}"
+  echo "argv: ${@}"
+	${bin} ${@}
+}
+
+assert_equal() {
+  if [[ "${1}" == "--eval" ]]; then
+    shift
+    expected=(${!1})
+    actual=(${!2})
+  else
+    expected="${1}"
+    actual="${2}"
+  fi
+  
+  diff=$(echo ${expected[@]} ${actual[@]} | tr ' ' '\n' | sort | uniq -u)
+
+  if [[ -z "${diff}" ]]; then
+  	return 0
+  else
+    echo "-- not equal"
+		echo "expected : ${expected[@]}"
+		echo "actual   : ${actual[@]}"
+		echo "diff     : ${diff[@]}"
+    echo "--"
+  	return 1
+  fi
+}
