12159c21737e27ea413386ff8ac1893768fc6212
Marcus Stade <marcus@stade.se>
2016-08-18 16:24:07 +0200

    Add unit test for javascript builder

---
+++ test/unit/builder/javascript.test.js
@@ -0,0 +1,37 @@
+import test from 'tape-async'
+import { loadUnit, readFixture } from '../test-util.js'
+import configure from '../../../lib/builder/javascript'
+
+test('JavaScript builder', async t => {
+  t.plan(7)
+
+  const pkg = await readFixture('typical-project')
+      , cfg = await loadUnit('lib/builder/javascript')
+
+  const opts =
+        { flags: {}
+        , lib: pkg.directories.lib
+        , src: pkg.directories.src
+        }
+
+  const srcMap = '\n//# sourceMappingURL=a.js.map'
+
+  t.comment('building with source maps')
+  opts.debug = true
+  let debugBuild = cfg(pkg, opts)
+  let { files: debug } = await debugBuild('a', `${pkg.root}/${pkg.directories.src}/a.js`)
+  t.ok(debug['a.js'], 'debug build includes a.js')
+  t.ok(debug['a.js.map'], 'debug build includes a.js.map')
+  t.equal(debug['a.js'].slice(-srcMap.length), srcMap, 'debug build has source map comment at the end')
+  
+  t.comment('building without source maps')
+  opts.debug = false
+  let releaseBuild = cfg(pkg, opts)
+  let { files: release } = await releaseBuild('a', `${pkg.root}/${pkg.directories.src}/a.js`)
+  t.ok(release['a.js'], 'build includes a.js')
+  t.notOk(release['a.js.map'], 'build does not include a.js.map')
+  t.notEqual(release['a.js'].slice(-srcMap.length), srcMap, 'build has no source map comment at the end')
+
+  t.comment('general assertions')
+  t.equal(debug['a.js'].slice(0, -srcMap.length), release['a.js'], 'without source map comment, builds should be equivalent')
+})
